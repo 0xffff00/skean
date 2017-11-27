967a36a539c5158ad58ae3ee3fff736fcaf40bb8
hzk <hzkchina@gmail.com>
2017-11-27 17:03:35 +0800

    [web]fix failed tests, round 4

--- skean-web-spring-boot-starter/src/test/java/party/threebody/skean/web/eg/navyapp/NavyTestApp.java
+++ skean-web-spring-boot-starter/src/test/java/party/threebody/skean/web/eg/navyapp/NavyTestApp.java
@@ -1,18 +1,10 @@
 package party.threebody.skean.web.eg.navyapp;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.util.UrlPathHelper;
 
 @SpringBootApplication
 public class NavyTestApp {
-
 
 
     public static void main(String[] args) {
